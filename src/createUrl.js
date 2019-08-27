de9e82c029b007ff811078806f1343368b96c460
yevhen-sahach <antitop04@gmail.com>
2019-08-27 13:10:39 +0300

    first commit

--- src/createUrl.js
+++ src/createUrl.js
@@ -17,7 +17,30 @@
  * @return {string} - created URL
  */
 function createUrl(template, params) {
-  // write code here
+  let url = template;
+  let openParentheses = 0;
+  let closeParentheses = 0;
+
+  for (let i = 0; i < url.length; i++) {
+    if (url[i] === '{') {
+      openParentheses = i;
+    }
+
+    if (url[i] === '}') {
+      closeParentheses = i;
+    }
+
+    if (closeParentheses > openParentheses) {
+      url = url
+        .replace(url.slice(openParentheses, closeParentheses + 1),
+          params[url.slice(openParentheses + 1, closeParentheses)]);
+      i = openParentheses;
+      closeParentheses = 0;
+      openParentheses = 0;
+    }
+  }
+
+  return url;
 }
 
 module.exports = createUrl;
