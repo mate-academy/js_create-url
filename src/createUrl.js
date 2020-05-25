a9be207f550315d105be08652194914162931d56
Liliia <chuiko1112@gmail.com>
2020-05-25 12:33:00 +0300

    solution

--- src/createUrl.js
+++ src/createUrl.js
@@ -10,14 +10,26 @@
  * createUrl('/api/{id}', {id: 0}) === '/api/0'
  * createUrl('/api/{id}', {name: 'Petya'}) === '/api/undefined'
  * createUrl('/api/{list}/{id}', {list: 'items', id: 0}) === '/api/items/0'
- *
- * @param {string} template
- * @param {object} params
- *
- * @return {string} - created URL
- */
+
+*
+* @param {string} template
+* @param {object} params
+*
+* @return {string} - created URL
+*/
 function createUrl(template, params) {
-  // write code here
+  const keysList = template.match(/(?<={)[a-z]{1,}(?=})/g);
+  let url = template;
+
+  for (let i = 0; i < keysList.length; i++) {
+    if (params[keysList[i]] !== undefined) {
+      url = url.replace(/\{[a-z]{1,}\}/, params[keysList[i]]);
+    } else {
+      url = url.replace(/\{[a-z]{1,}\}/, undefined);
+    };
+  }
+
+  return url;
 }
 
 module.exports = createUrl;
